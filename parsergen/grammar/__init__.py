85891a16069789782381cd85a7ce93b27df37632
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-30 17:22:30 +0300

    Add exceptions for basic grammar rules violations

--- parsergen/grammar/__init__.py
+++ parsergen/grammar/__init__.py
@@ -10,7 +10,18 @@ Terminal = TypeVar("Terminal")
 Nonterminal = TypeVar("Nonterminal", bound=NonTerminalBase)
 
 
-@dataclass
+class InvalidStartSymbol(Exception):
+    def __init__(self, symbol) -> None:
+        msg = f"Symbol {symbol} can't be a start symbol because it is not in nonterminals set."
+        super().__init__(msg)
+
+
+class InvalidProduction(Exception):
+    def __init__(self, production, violator):
+        msg = f"Production {production} is incorrect due to contains symbol {violator} which is not in terminals and nonterminals sets."
+        super().__init__(msg)
+
+
 class Grammar(Generic[Terminal, Nonterminal]):
     terminals: Set[Terminal]
     nonterminals: Set[Nonterminal]
